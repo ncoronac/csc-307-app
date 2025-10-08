e0a8125a09fde0a1e1c1511ad3afff3a764132f2
Noemi Corona Calvario <coronanoemi177@gmail.com>
2025-10-07 21:26:36 -0700

    CORS

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,10 +1,12 @@
 // backend.js
 
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
 
 const users = {

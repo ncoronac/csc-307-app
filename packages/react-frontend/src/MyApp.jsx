590c568119c569ec1ab2b392c9b9c3f28475faa0
Noemi Corona Calvario <coronanoemi177@gmail.com>
2025-10-07 22:02:34 -0700

    connecting submit button and backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -33,7 +33,23 @@ function MyApp() {
     }
 
     function updateList(person) {
-        setCharacters([...characters, person]);
+        postUser(person)
+        .then(() => setCharacters([...characters, person]))
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    function postUser(person) {
+        const promise = fetch("http://localhost:8000/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(person),
+        });
+
+        return promise;
     }
 
     useEffect(() => {
